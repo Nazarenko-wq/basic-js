2ebb99cfd2f585284a2a7ad7d95f584f044cab5a
Nazarenko Michael <mihaaaaa435@gmail.com>
2022-10-13 11:48:24 +0300

    feat: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
+  let result = [];
+
+  for(let elem of members) {
+    if(typeof(elem) == 'string') {
+        for(let word of elem.split(' ')) {
+            result.push(word[0].toUpperCase());
+        }
+    } else {
+        return false;
+    }
+  }
+  return result.sort().join('');
+    
 }
 
 module.exports = {
