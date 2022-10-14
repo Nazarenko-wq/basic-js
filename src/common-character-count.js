b36e8332e44efcdea8067a95ddd59514d93674bd
Nazarenko Michael <mihaaaaa435@gmail.com>
2022-10-14 10:45:16 +0300

    feat: ku

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,22 +12,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-  // throw new NotImplementedError('Not implemented');
-  let arr1 = s1.split('');
-  let arr2 = s2.split('');
-  let counter = 0;
+  throw new NotImplementedError('Not implemented');
+  // let arr1 = s1.split('');
+  // let arr2 = s2.split('');
+  // let counter = 0;
   
-  for(let i = 0; i < arr1.length; i++) {
-      for(let j = 0; j < arr2.length; j++) {
-          if(arr1[i] == arr2[j]) {
-              counter++;
-              arr1.splice(arr1[i], 1);
-              arr2.splice(arr2[j], 1);
-          }
-      }
-  }
+  // for(let i = 0; i < arr1.length; i++) {
+  //     for(let j = 0; j < arr2.length; j++) {
+  //         if(arr1[i] == arr2[j]) {
+  //             counter++;
+  //             arr1.splice(arr1[i], 1);
+  //             arr2.splice(arr2[j], 1);
+  //         }
+  //     }
+  // }
 
-  return counter;
+  // return counter;
 }
 
 module.exports = {
